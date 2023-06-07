e793895fdb051d21d625f9eacc889ca14e27504d
Daniel Kuku <cristobaldaniel022@gmail.com>
2023-06-07 09:07:04 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const list = document.querySelector(".products-list");
+const search = document.querySelector(".search input");
+
+const filterTodos = (term) => {
+  Array.from(list.children)
+    .filter((todo) => !todo.textContent.toLowerCase().includes(term))
+    .forEach((todo) => todo.classList.add("filtered"));
+
+  Array.from(list.children)
+    .filter((todo) => todo.textContent.toLowerCase().includes(term))
+    .forEach((todo) => todo.classList.remove("filtered"));
+};
+
+//keyup event
+search.addEventListener("keyup", () => {
+  const term = search.value.trim().toLowerCase();
+  filterTodos(term);
+});
